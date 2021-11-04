518422e6555d0c40c26da1be7e09da214cccc612
ralexrivero <ralexrivero@gmail.com>
2021-11-03 21:42:08 -0300

    simply match

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Ruby script that acepts one argument and pass it to a regex metching method
+puts ARGV[0].scan(/School/).join
---
+++ 0x06-regular_expressions/run_scripts.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./0-simply_match_school.rb School | cat -e
