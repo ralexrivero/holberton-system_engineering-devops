756baa836d858fb0ea898a264bc27061d7d9aa5b
ralexrivero <ralexrivero@gmail.com>
2021-11-03 23:18:15 -0300

    syntax error

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # Ruby script that use regex with match: start with `h` and ends with `n` and any single character in between
-puts ARGV[0].scan(//).join
+puts ARGV[0].scan(/^h.n$/).join
