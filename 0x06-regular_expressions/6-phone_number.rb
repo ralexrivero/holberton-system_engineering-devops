f50cc964cd33742b6ddffc3df15306855be20392
ralexrivero <ralexrivero@gmail.com>
2021-11-03 23:31:40 -0300

    change mode bits permision to allow execution

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# Ruby script that matches only 10 digits phone number
+# Ruby script that matches only 10 digits phone number without other character
 puts ARGV[0].scan(/^\d{10}$/).join
