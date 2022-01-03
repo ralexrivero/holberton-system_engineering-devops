e1b2ab9d359ab57a2f6bf188716fbb4edfe5fff7
ralexrivero <ralexrivero@gmail.com>
2022-01-03 19:38:45 -0300

    fix group name syntax error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create file
 file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+    ensure  => 'file',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
