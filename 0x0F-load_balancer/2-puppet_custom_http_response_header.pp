2a6919cd4dae9e441ce16dc5ff72d3e8f02de90f
ralexrivero <ralexrivero@gmail.com>
2022-01-21 22:39:47 -0300

    config http header in nginx server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,19 @@
 # HTTP header response, name X-Served-By, value of the custom HTTP header must be the hostname of the server Nginx is running on
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
+}
+-> package {'nginx':
+  ensure => present,
+}
+-> file_line { 'header line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
+}
+-> exec { 'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
